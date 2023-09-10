847fe664c8ee63fa57bec90ae31b6c760574075d
salomemr <etnies.manriquer@uqvirtual.edu.co>
2023-09-10 14:36:06 -0500

    "test: realizan dos features v1"

---
+++ taller_apirest_salome_version/features/steps/listado_paginado_steps.py
@@ -0,0 +1,109 @@
+from behave import given, when, then, step
+import psycopg2
+
+
+db_config = {
+    # Configuración de la base de datos
+    'dbname': 'authentication_db',
+    'user': 'admin',
+    'password': 'admin_password',
+    'host': 'localhost'
+}
+
+@given('establecer la conexion con la base de datos')
+def establecer_conexion_bd(context):  
+    # Establecer la conexión con la base de datos
+    conn = psycopg2.connect(**db_config)
+    # Guardar la conexión en el contexto para su uso posterior
+    context.db_connection = conn
+    cursor = context.db_connection.cursor()
+    cursor.execute("SELECT * FROM users")
+    user = cursor.fetchone()
+    context.email=user[3]
+    context.user=user
+
+
+@when('proporcionar los parametros de paginacion {pagina} con un tamaño de página de {tamano_pagina}')
+def solicitar_lista_de_usuarios(context, pagina, tamano_pagina):
+    # Convierte los valores de página y tamaño de página en enteros
+    pagina = int(pagina)
+    tamano_pagina = int(tamano_pagina)
+
+    # Verifica si los valores son válidos (mayores o iguales a 1)
+    if pagina < 1 or tamano_pagina < 1:
+        context.error_message = "Los parámetros de paginación deben ser mayores o iguales a 1"
+    else:
+        # Recuperar la conexión a la base de datos del contexto
+        conn = context.db_connection
+
+        # Crear un cursor para ejecutar consultas
+        cursor = conn.cursor()
+
+        # Calcular el desplazamiento basado en la página y el tamaño de la página
+        offset = (pagina - 1) * tamano_pagina
+
+        # Consulta SQL para obtener la lista de usuarios con paginación
+        query = f"SELECT * FROM users LIMIT {tamano_pagina} OFFSET {offset}"
+
+        # Ejecutar la consulta
+        cursor.execute(query)
+
+        # Obtener la lista de usuarios en la página actual
+        users = cursor.fetchall()
+
+        # Guardar la lista de usuarios en el contexto para su uso posterior
+        context.paginated_users = users
+
+        # Cerrar el cursor y la conexión
+        cursor.close()
+        conn.close()
+    # # Convierte los valores de página y tamaño de página en enteros
+    # pagina = int(pagina)
+    # tamano_pagina = int(tamano_pagina)
+
+    # if pagina < 1 or tamano_pagina < 1:
+    #     pass
+
+    # # Recuperar la conexión a la base de datos del contexto
+    # conn = context.db_connection
+
+    # # Crear un cursor para ejecutar consultas
+    # cursor = conn.cursor()
+
+    # # Calcular el desplazamiento basado en la página y el tamaño de la página
+    # offset = (pagina - 1) * tamano_pagina
+
+    # # Consulta SQL para obtener la lista de usuarios con paginación
+    # query = f"SELECT * FROM users LIMIT {tamano_pagina} OFFSET {offset}"
+
+    # # Ejecutar la consulta
+    # cursor.execute(query)
+
+    # # Obtener la lista de usuarios en la página actual
+    # users = cursor.fetchall()
+
+    # # Guardar la lista de usuarios en el contexto para su uso posterior
+    # context.paginated_users = users
+
+    # # Cerrar el cursor y la conexión
+    # cursor.close()
+    # conn.close()
+
+@then('se recibe una respuesta con "{codigo}"')
+def enviar_reporte_codigo_p(context,codigo):
+    assert codigo==codigo
+
+        
+@then('la respuesta contiene una lista de {cantidad} usuarios')
+def verificar_lista_de_usuarios(context, cantidad):
+    # Convierte la cantidad deseada en un entero
+    cantidad = int(cantidad)
+
+    # Verifica que la respuesta contenga la cantidad correcta de usuarios
+    assert len(context.paginated_users) == cantidad
+
+
+
+@then('la respuesta contiene un mensaje "{mensaje}"')
+def enviar_reporte_mensaje_p(context,mensaje):
+    assert mensaje==mensaje
--- taller_apirest_salome_version/features/steps/restablecimiento_contrasena_steps.py
+++ taller_apirest_salome_version/features/steps/restablecimiento_contrasena_steps.py
@@ -1,62 +1,68 @@
-# from behave import given, when, then, step
-# import psycopg2
+from behave import given, when, then, step
+from behave import *
+import psycopg2
 
-# db_config = {
-#     # Configuración de la base de datos
-#     'dbname': 'authentication_db',
-#     'user': 'admin',
-#     'password': 'admin_password',
-#     'host': 'localhost'
-# }
+db_config = {
+    # Configuración de la base de datos
+    'dbname': 'authentication_db',
+    'user': 'admin',
+    'password': 'admin_password',
+    'host': 'localhost'
+}
 
-# @given('el usuario se encuentra ya registrado')
-# def establecer_existencia_usuario(context):
-#     # Establecer la conexión con la base de datos
-#     conn = psycopg2.connect(**db_config)
-#     # Guardar la conexión en el contexto para su uso posterior
-#     context.db_connection = conn
-#     cursor = context.db_connection.cursor()
-#     cursor.execute("SELECT * FROM users LIMIT 1")
-#     user = cursor.fetchone()
-#     context.email=user[3]
-#     context.user=user
-#     # Aseguro el usuario es diferente de null
-#     assert user!=None
+@given('el usuario se encuentra ya registrado')
+def establecer_existencia_usuario(context):
+    # Establecer la conexión con la base de datos
+    conn = psycopg2.connect(**db_config)
+    # Guardar la conexión en el contexto para su uso posterior
+    context.db_connection = conn
+    cursor = context.db_connection.cursor()
+    cursor.execute("SELECT * FROM users LIMIT 1")
+    user = cursor.fetchone()
+    context.email=user[3]
+    context.user=user
+    # Aseguro el usuario es diferente de null
+    assert user!=None
 
-# @when('el usuario proporciona el token, el correo electrónico y una nueva contraseña')
-# def datos_usuario(context):
-#     context.token = "ahfbasfajfb13413"
-#     context.email = "cristian_r@email.com"
-#     context.new_password = "password_nueva"
+@when('el usuario proporciona el token, el correo electrónico y una nueva contraseña')
+def datos_usuario(context):
+    context.token = "ahfbasfajfb13413"
+    context.email = "cristian_r@email.com"
+    context.new_password = "password_nueva"
     
+@when('el usuario proporciona un token expirado, el correo electrónico y una nueva contraseña')
+def datos_usuario(context):
+    context.token = "ahfbasfajfb13413"
+    context.email = "cristian_r@email.com"
+    context.new_password = "password_nueva"
 
-# @when('hacer la solicitud a el servidor que actualice la contrasena')
-# def step_impl(context):
-#     if (context.password != "password_valido") and (context.user!=None) :
-#        cursor = context.db_connection.cursor()
-#        # Actualizar la contraseña del usuario en la base de datos
-#        cursor.execute("UPDATE users SET hashed_password = %s WHERE email = %s",
-#                       (context.nuevo_password, context.current_user_email))
-#        pass
-#     else:
-#        pass
-        
+@when('el usuario proporciona un token que no existe, el correo electrónico y una nueva contraseña')
+def datos_usuario(context):
+    context.token = "ahfbasfajfb13413"
+    context.email = "cristian_r@email.com"
+    context.new_password = "password_nueva"
 
-# @then('la respuesta debería tener un código de estado  "{codigo}"')
-# def enviar_reporte(context,codigo):
-#     if context.user == 1 :
-#         assert codigo==codigo
-#     else:
-#         assert codigo==codigo
-
-        
-# @then('se captura el mensaje de respuesta "{mensaje}"')
-# def enviar_reporte(context,mensaje):
-#     if context.salida == 1 :
-#         assert mensaje==mensaje
-#     else:
-#         assert mensaje==mensaje
-            
+@when('hacer la solicitud al servidor que actualice la contrasena')
+def solictud_servidor(context):
+    if (context.new_password != "password_valido") and (context.user!=None) :
+        cursor = context.db_connection.cursor()
+        # Actualizar la contraseña del usuario en la base de datos
+        cursor.execute("UPDATE users SET hashed_password = %s WHERE email = %s",
+                       (context.new_password , context.email))
+        pass
+    else:
+        pass
         
         
+        
+@then('la respuesta debería tener un "{codigo}"') 
+def enviar_reporte_codigo_res(context,codigo):
+    if context.user == 1 :
+        assert codigo==codigo
+    else:
+        assert codigo==codigo
 
+@then('se captura el mensaje de respuesta "{mensaje}"')
+def enviar_reporte_mensaje_res(context,mensaje):
+    assert mensaje==mensaje
+    
