b892e9ad627bd7d3c4fcfdc17ef2b5cc8ab34570
Canbril <carlos990720@gmail.com>
2023-09-11 23:18:31 -0500

    Creacion steps y feature de actualizacion_usuario

---
+++ taller_apirest_salome_version/features/actualizacion_usuario.feature
@@ -0,0 +1,23 @@
+Feature: Obtencion de usuarios
+
+    #Este caso de prueba se enfocará en que las credenciales son validas
+    Scenario: Obtener los datos del usuario con una id valida
+
+        #Requerimientos:
+        Given tener un usuario en sesion
+        And tener un correo valido
+            #Acción
+            When hacer la solicitud al servidor que actualice el correo
+            #Resultado esperado
+            Then se muestra el mensaje que retorna el server "Usuario actualizada exitosamente    status code:200"
+
+    #Este caso se basa en que se haya seleccionado un usuario con id no es valida
+    Scenario: Obtener los datos del usuario con una id invalidas
+
+        #Requerimientos:
+        Given no tener un usuario en sesion
+        And tener un correo no valido
+            #Acción
+            When hacer la solicitud al servidor que actualice el correo
+            #Resultado esperado
+            Then se muestra el mensaje que retorna el server "error     status code:400"
---
+++ taller_apirest_salome_version/features/steps/actualizacion_usuario_steps.py
@@ -0,0 +1,77 @@
+from behave import given, when, then, step
+from flask_jwt_extended import get_jwt_identity
+import psycopg2
+import jwt
+
+db_config = {
+    # Configuración de la base de datos
+    'dbname': 'authentication_db',
+    'user': 'admin',
+    'password': 'admin_password',
+    'host': 'localhost'
+}
+
+
+
+
+@given('tener un usuario en sesion')
+def establecer_conexion(context):  
+    # Establecer la conexión con la base de datos
+    conn = psycopg2.connect(**db_config)
+    # Guardar la conexión en el contexto para su uso posterior
+    context.db_connection = conn
+    cursor = context.db_connection.cursor()
+    cursor.execute("SELECT * FROM users LIMIT 1")
+    user = cursor.fetchone()
+    context.email=user[3]
+    context.user=user
+    # Aseguro el usuario es diferente de null
+    assert user!=None
+    
+@given('no tener un un usuario en sesion')
+def establecer_conexion(context):  
+    context.user=None
+    # Aseguro el usuario es diferente de null
+    assert context.user==None
+        
+    
+@given('tener una contrasena valida')
+def establecer_conexion(context):  
+    context.email = "email_valido@email.com"
+    # Aseguro el usuario es diferente de null
+    assert context.email=="email_valido@email.com"
+    
+@given('tener una contrasena no valida')
+def establecer_conexion(context):  
+    context.email = "email_novalido@enail.com"
+    # Aseguro el usuario es diferente de null
+    assert context.email=="email_novalido@enail.com"
+    
+
+    
+@when('hacer la solicitud a el servidor que actualice el correo')
+def step_impl(context):
+    if (context.email != "email_valido@email.com") and (context.user!=None) :
+       cursor = context.db_connection.cursor()
+       # Actualizar la contraseña del usuario en la base de datos
+       cursor.execute("UPDATE users SET email = %s WHERE email = %s",
+                       (context.new_email, context.current_user_email))
+       pass
+    else:
+       pass
+        
+
+    
+    
+@then('se muestra el mensaje que retorna el server "{mensaje}"')
+def enviar_reporte(context,mensaje):
+    if context.user!=None :
+        assert mensaje==mensaje
+    else:
+        assert mensaje==mensaje
+    
+
+    
+    
+
+
